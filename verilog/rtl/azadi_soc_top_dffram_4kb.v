0f91768be0eea1f7505e43b074325d99b11b76bc
zeeshanrafique23 <zeeshanrafique23@gmail.com>
2021-06-24 01:18:30 +0500

    updated files with SPDX header

--- verilog/rtl/azadi_soc_top_dffram_4kb.v
+++ verilog/rtl/azadi_soc_top_dffram_4kb.v
@@ -1,5 +1,23 @@
-/*azadi_soc_top_conv-v0.6*/
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-License-Identifier: Apache-2.0
+   
+// Designed by a Team at Micro Electronics Research Lab, Usman Institute of Technology.
+// https://www.merledupk.org
 
+/*azadi_soc_top_conv-v0.6*/
 `default_nettype wire
 module azadi_soc_top (
 	`ifdef USE_POWER_PINS
